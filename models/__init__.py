db94df4a3cedf27ce0f3ba65efaaf4e5ef9938e9
Hadi Ibrahim <17hadiibrahim@gmail.com>
2023-09-02 22:51:43 +0200

    first draft of epic kitchen script

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,5 @@ from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import Classifier
 # from models.GRU import GRU
-# from models.LSTM import LSTM
+from models.LSTM import LSTM
 # from models.Transformer import Transformer
